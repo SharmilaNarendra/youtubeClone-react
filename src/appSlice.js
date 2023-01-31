c127de056352ea631dc2dd46382f0bf0fcc819ac
Sharmila Narendra Babu <86154019+SharmilaNarendra@users.noreply.github.com>
2023-01-31 15:05:19 +0530

    feat: youTube panel header and vedios cards

---
+++ src/appSlice.js
@@ -0,0 +1,18 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+const appSlice= createSlice({
+
+    name:"appSlice",
+    initialState:{
+        openMenu: false,
+    },
+    reducers:{
+      toggleMenu: (state)=>{
+       state.openMenu = !state.openMenu
+      },
+    }
+});
+
+export const {toggleMenu} = appSlice.actions;
+
+export default appSlice.reducer;
